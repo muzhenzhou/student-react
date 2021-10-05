63fc8fc798f522b6620d4c499c988af44b5cd40f
被窝探险家 <shirley@192.168.0.101>
2021-10-05 15:19:46 +0800

    update

--- src/Components/AddStudent.js
+++ src/Components/AddStudent.js
@@ -1,56 +1,107 @@
 import React, {Component} from 'react'
 
 class AddStudent extends Component {
+    constructor() {
+        super()
+        this.stateHandler = this.stateHandler.bind(this)
+    }
+
+    state = {
+        number: '',
+        name: '',
+        sex: '女',
+        age: '',
+        time: '',
+        college: '大前端',
+        hobbies: [
+            {
+                id: 1,
+                title: '篮球',
+                isChecked: false
+            },
+            {
+                id: 2,
+                title: '足球',
+                isChecked: false
+            },
+            {
+                id: 3,
+                title: '网球',
+                isChecked: false
+            }
+        ]
+    }
+
+    stateHandler(e) {
+        const value = e.target.value
+        const prop = e.target.name
+
+        this.setState({
+            [prop]: value
+        }, () => {
+            console.log(this.state)
+        })
+    }
+
+    hobbyHandler(index, ev) {
+        const isChecked = ev.target.checked
+        const hobbies = [...this.state.hobbies]
+        hobbies[index].isChecked = isChecked
+        this.setState({hobbies}, () => {
+            console.log(this.state.hobbies)
+        })
+    }
+
+    submit = (ev) => {
+        ev.preventDefault()
+        console.log(this.state)
+    }
     render() {
       return(
         <div className="col-md-5">
-            <form>
+            <form onSubmit={this.submit}>
                 <div className="form-group">
                     <label>学号</label>
-                    <input type="number" className="form-control" placeholder="请输入学号" />
+                    <input name={'number'} value={this.state.number} onChange={this.stateHandler} type="number" className="form-control" placeholder="请输入学号" />
                 </div>
                 <div className="form-group">
                     <label>姓名</label>
-                    <input type="text" className="form-control" placeholder="请输入姓名" />
+                    <input name={'name'} value={this.state.name} onChange={this.stateHandler} type="text" className="form-control" placeholder="请输入姓名" />
                 </div>
                 <div className="form-group">
                     <label>性别&nbsp;&nbsp;</label>
                     <label className="checkbox-inline">
-                        <input type="radio" value="男" /> 男
+                        <input name='sex' defaultChecked={this.state.sex === '男'} onChange={this.stateHandler} type="radio" value="男" /> 男
                     </label>
                     <label className="checkbox-inline">
-                        <input type="radio" value="女" /> 女
+                        <input name='sex' defaultChecked={this.state.sex === '女'} onChange={this.stateHandler} type="radio" value="女" /> 女
                     </label>
                 </div>
                 <div className="form-group">
                     <label>年龄</label>
-                    <input type="text" className="form-control" placeholder="请输入姓名" />
+                    <input name={'age'} value={this.state.age} onChange={this.stateHandler} type="text" className="form-control" placeholder="请输入年龄" />
                 </div>
                 <div className="form-group">
                     <label>入学时间</label>
-                    <input className="form-control" type="date" />
+                    <input name={'time'} value={this.state.time} onChange={this.stateHandler} className="form-control" type="date" />
                 </div>
                 <div className="form-group">
                     <label>爱好</label>
-                    <div className="checkbox">
-                        <label>
-                            <input type="checkbox" value="足球" /> 足球
-                        </label>
-                    </div>
-                    <div className="checkbox">
-                        <label>
-                            <input type="checkbox" value="篮球" /> 篮球
-                        </label>
-                    </div>
-                    <div className="checkbox">
-                        <label>
-                            <input type="checkbox" value="橄榄球" /> 橄榄球
-                        </label>
-                    </div>
+                    {
+                        this.state.hobbies.map((hobby, index) => {
+                            return (
+                                <div className="checkbox" key={hobby.id}>
+                                    <label>
+                                        <input type="checkbox" defaultChecked={hobby.isChecked} value={hobby.title} onChange={this.hobbyHandler.bind(this, index)} /> {hobby.title}
+                                    </label>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
                 <div className="form-group">
                     <label>所属学院</label>
-                    <select className="form-control">
+                    <select name={'college'} value={this.state.college} onChange={this.stateHandler} className="form-control">
                         <option value="大前端">大前端</option>
                         <option value="Java">Java</option>
                         <option value="python">python</option>
